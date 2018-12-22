20274a9b2afcf658e51dd23da09efbf4eded34ae
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2018-12-22 20:18:30 +0300

    Redux fix, dont work

---
+++ src/containers/Forum/Forum.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { getData, setFilter } from '../../actions'
+import connect from 'react-redux/es/connect/connect'
+
+class Forum extends Component{
+  render(){
+    const {
+      ...props
+    } = this.props
+    return(
+      <Forum {...props}/>
+    )
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    products: state.products,
+    filter: state.filter
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getData: bindActionCreators(getData, dispatch),
+    setFilter: bindActionCreators(setFilter, dispatch)
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Forum)
