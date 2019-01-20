ce9592a8ec51ea06ec490091dd97b274ba50f00c
f0r3st <adgerom11@gmail.com>
2019-01-20 12:13:52 +0300

    fix

--- src/screens/PostScreen/PostScreen.js
+++ src/screens/PostScreen/PostScreen.js
@@ -1,49 +1,52 @@
 import React from 'react'
-import { ScrollView, View } from 'react-native'
-import { Text, Image } from 'react-native'
+import {ScrollView, View} from 'react-native'
+import {Text, Image} from 'react-native'
 import CommentAdd from '../../components/Comment/CommentAdd'
 import styles from './scss/style.scss'
 import _ from 'lodash'
 import * as arrd from '../../constants/addr'
 
+// TODO Сделать сжатие отправляемых фото
 class PostScreen extends React.Component {
-  render () {
-    const {
-      props: {
-        item,
-        country
-      }
-    } = this
-    let curCountry = _.find(country, x => x.Id === item.CountryId)
-    curCountry = curCountry ? curCountry.Name : 'Другая'
+    render() {
+        const {
+            props: {
+                item,
+                country
+            }
+        } = this
+        let curCountry = _.find(country, x => x.Id === item.CountryId)
+        curCountry = curCountry ? curCountry.Name : 'Другая'
 
-    const imageAddr = arrd.IMAGES_SERVER + item.Image
-    console.log(imageAddr)
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Image
-            style={{width: 300, height: 300}}
-            source={{uri: imageAddr}}
-          />
-          <Text>{item.Image}</Text>
-          <Text>id = {item.Id}</Text>
-          <Text>{item.Title}</Text>
-          <Text>{item.Text}</Text>
-          <Text>страна: {curCountry}</Text>
-        </View>
-        {/*<CommentAdd />*/}
+        const imageAddr = arrd.IMAGES_SERVER + item.Image
+        console.log(imageAddr)
+        return (
+            <ScrollView>
+                <View style={{alignItems: 'center'}}>
 
-      </ScrollView>
-    )
-  }
+                    <Text style={styles.title}>{item.Title}</Text>
+                    <Image
+                        style={styles.img}
+                        source={{uri: imageAddr}}
+                    />
+                </View>
+                <View style={styles.text}>
+                    <Text>Cтрана: {curCountry}</Text>
+                    <Text style={styles.comment}>{item.Text}</Text>
 
-  static defaultProps = {
-    Id: 'Пусто',
-    Title: 'Пусто',
-    curCountry: 'Другая',
-    Text: 'Пусто'
-  }
+                </View>
+                {/*<CommentAdd />*/}
+
+            </ScrollView>
+        )
+    }
+
+    static defaultProps = {
+        Id: 'Пусто',
+        Title: 'Пусто',
+        curCountry: 'Другая',
+        Text: 'Пусто'
+    }
 }
 
 export default PostScreen
